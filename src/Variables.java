73d526674097afcc4e890e67cd7150772fa81675
Deven S <devenseth8@gmail.com>
2023-01-30 19:15:10 -0500

    Project Init

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int intOperandA = 1;
+        int intOperandB = 5;
+        int intSum = 13;
+        int intProduct = 3*3;
+        int intDifference = 6;
+        int intQuotient = 7;
+        int intModulo = 4;
+    }
+}
