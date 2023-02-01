6ec0cec661fffb11df83f4d5440ecc7f71c8711d
Deven S <devenseth8@gmail.com>
2023-01-31 19:25:30 -0500

    Project Init

--- src/Variables.java
+++ src/Variables.java
@@ -1,11 +1,17 @@
 public class Variables {
     public static void main(String[] args) {
-        int intOperandA = 1;
-        int intOperandB = 5;
-        int intSum = 13;
-        int intProduct = 3*3;
-        int intDifference = 6;
-        int intQuotient = 7;
-        int intModulo = 4;
+        int intOperandA = 1; //This is a declaration and initial assignment of an integer variable
+        int intOperandB = 5; //This is a declaration and initial assignment of an integer variable
+        int intSum = 13; //This is a declaration and initial assignment of an integer variable
+        int intProduct = 3; //This is a declaration and initial assignment of an integer variable
+        int intDifference = 6; //This is a declaration and initial assignment of an integer variable
+        int intQuotient = 7; //This is a declaration and initial assignment of an integer variable
+        int intModulo = 4; //This is a declaration and initial assignment of an integer variable
+        double doubleOperandA = 1.1; //This is a declaration and initial assignment of a double variable
+        double doubleOperandB = 5.2; //This is a declaration and initial assignment of a double variable
+        double doubleSum = 13.3; //This is a declaration and initial assignment of a double variable
+        double doubleProduct = 3.4; //This is a declaration and initial assignment of a double variable
+        double doubleDifference = 6.5; //This is a declaration and initial assignment of a double variable
+        double doubleQuotient = 7.6; //This is a declaration and initial assignment of a double variable
     }
 }
