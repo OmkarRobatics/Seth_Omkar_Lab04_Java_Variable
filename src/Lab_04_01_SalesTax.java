40011c0c9fc50dd49e23e6b113b910b66105215c
Omkar Seth <omkarseth008@gmail.com>
2023-02-05 12:08:33 -0500

    Project Init

--- src/Lab_04_01_SalesTax.java
+++ src/Lab_04_01_SalesTax.java
@@ -1,6 +1,6 @@
 public class Lab_04_01_SalesTax {
     public static void main(String[] args) {
-        double purchasePrice = 20.0; // inputs the purchase price (input is not made but selected by me)
+        double purchasePrice = 20.0; // inputs the purchase price (input is not made but a number is selected by me)
         double salesTax = purchasePrice*0.05; // finds the sales tax and stores it
         double totalPrice = purchasePrice + salesTax; // total price with sales tax is found
         System.out.println("The purchase price entered was $" + purchasePrice); // original purchase price is printed
